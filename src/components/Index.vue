c82e803a8a4e2fcf673fdda1f061a7058779de9d
13d_100006830770_fanwenbo <13d_100006830770_fanwenbo@git.cloud.tencent.com>
2018-08-24 18:26:42 +0800

    second commit

---
+++ src/components/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="index">
+    rereree
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
